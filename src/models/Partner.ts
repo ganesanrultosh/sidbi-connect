49f31bd4a3b2c848d9b3a961926409edf81ee1b7
Ganesan <ganesan@Ganesans-MacBook-Pro-2.local>
2023-09-28 16:58:17 +0530

    Added partner model

---
+++ src/models/Partner.ts
@@ -0,0 +1,16 @@
+interface Partner {
+    username: string | undefined;
+    password: string | undefined;
+    confirmPassword: string| undefined;
+    name: string | undefined;
+    mobileNo: string | undefined;
+    pan: string | undefined;
+    keyPerson: string | undefined;
+    address: string | undefined;
+    city: string | undefined;
+    state: string | undefined;
+    pinCode: number | undefined;
+    type: string | undefined;
+    category: string | undefined;
+    subCategory: string | undefined;
+}
