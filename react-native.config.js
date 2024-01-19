adcf1f293a6443dcf591ac93088c2b7240f952c4
ganesan <ganesan@ganesans-MacBook-Pro-2.local>
2024-01-19 14:25:23 +0530

    icon issue fixed

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   dependencies: {
-  ...(process.env.NO_FLIPPER ? { 'react-native-flipper': { platforms: { ios: null } } } : {}),
-  }}
+    ...(process.env.NO_FLIPPER ? 
+      { 'react-native-flipper': 
+        { platforms: { ios: null } } } : {}),
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+    },
+  }
+}
